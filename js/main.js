7328946ac073ed936ef45cfc348325158ce43198
KevinTib2024 <ktibaquicha@ucundinamarca.edu.co>
2025-05-26 02:36:07 -0500

    Primera subida limpia con LFS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+<main class="content">
+
+<section class="unity-download">
+  <h2>Descarga la Maqueta Interactiva</h2>
+  <p>A continuación puedes descargar la maqueta desarrollada en Unity que representa la solución propuesta:</p>
+  <a href="descargas/maqueta-unity.zip" class="download-button" download>
+    Descargar Maqueta (.zip)
+  </a>
+</section>
+
+</main>
